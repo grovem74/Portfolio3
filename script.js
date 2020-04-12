9cb4d87fcdce7849552994897b868df4b4946001
grovem74 <matthewg74@gmail.com>
2020-04-12 13:58:56 -0600

    added projects to portfolio and made some style adjustments

--- script.js
+++ script.js
@@ -4,28 +4,4 @@ document.addEventListener('DOMContentLoaded', function() {
     var instances = M.Sidenav.init(elems);
   });
 
-  // $("#about-mobile").on("click", function (){
-  //   $("#about").attr("style", "padding: 50% 0 80% 0");
-  // });
-
-  // $("#contact-mobile").on("click", function (){
-  //   $("#contact").attr("style", "padding: 40% 0 60% 0");
-  // });
-
-  // $("#skills-mobile").on("click", function (){
-  //   $("#skills-title").attr("style", "bottom: 5.5em");
-  //   $("#skills").attr("style", "padding-top: 80%");
-  // });
-  
-  // $("#about-link").on("click", function (){
-  //   $("#about").attr("style", "padding: 50% 0 80% 0");
-  // });
-
-  // $("#contact-link").on("click", function (){
-  //   $("#contact").attr("style", "padding: 40% 0 80% 0");
-  // });
-
-  // $("#skills-link").on("click", function (){
-  //   $("#skills").attr("style", "padding: 60% 0");
-  // });
 
